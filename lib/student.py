f4193dacf6dca07c66b2af6f0aec400ee17a8f44
Dave J <29786887+daveswork@users.noreply.github.com>
2024-08-28 14:57:48 -0400

    All local tests pass.

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,17 @@
 class Student:
-    pass
+    
+    def hello(self):
+        print("Hey there! I'm so excited to learn stuff.")
+    
+    def raise_hand(self):
+        print("Pick me!")
 
 class ChattyStudent(Student):
-    pass
+    
+    def hello(self):
+        chatty_phrase = "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+        return super().hello(), print(chatty_phrase)
+    
+    def raise_hand(self):
+        for i in range(10):
+            super().raise_hand()
